f25c4d62f8a21f75015b8c37963b2d06531be667
Adrian <adriansanduta@gmail.com>
2025-03-15 19:00:49 -0500

    clock1.5

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const secondHandle = document.querySelector('.handle__seconds');
 const minuteHandle = document.querySelector('.handle__minutes');
 const hourHandle = document.querySelector('.handle__hours');
 
-const d = new Date();   
-const hour = d.getHours();
-const minutes = d.getMinutes();
-const seconds = d.getSeconds();
+requestAnimationFrame(setHandles);
 
-hourHandle.style.transform = `translateX(-50%) rotate(${hour * 30}deg)`;
-minuteHandle.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`;    
-secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`;
+
+function setHandles() {
+ const d = new Date();   
+ const hour = d.getHours();
+ const minutes = d.getMinutes();
+ const seconds = d.getSeconds();
+
+ hourHandle.style.transform = `translateX(-50%) rotate(${hour * 30}deg)`;
+ minuteHandle.style.transform = `translateX(-50%) rotate(${minutes * 6}deg)`;    
+ secondHandle.style.transform = `translateX(-50%) rotate(${seconds * 6}deg)`;
+
+    requestAnimationFrame(setHandles);
+}
